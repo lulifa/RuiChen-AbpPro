70fdbeeb705795c7e7d654e328958e560df4da07
lulifa <814570123@qq.com>
2024-08-26 14:47:58 +0800

    feature：adjust controller code

---
+++ aspnet-core/services/RuiChen.AbpPro.Admin.HttpApi.Host/Controllers/UserSettingMergeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using RuiChen.AbpPro.SettingManagement;
+using Volo.Abp.DependencyInjection;
+
+namespace RuiChen.AbpPro.Admin.HttpApi.Host
+{
+    [ExposeServices(
+    typeof(UserSettingController),
+    typeof(UserSettingMergeController))]
+    public class UserSettingMergeController : UserSettingController
+    {
+        private readonly SettingManagementMergeOptions _mergeOptions;
+        public UserSettingMergeController(
+            IUserSettingAppService service,
+            IOptions<SettingManagementMergeOptions> mergeOptions)
+            : base(service)
+        {
+            _mergeOptions = mergeOptions.Value;
+        }
+
+        [HttpGet]
+        [Route("by-current-user")]
+        public async override Task<SettingGroupResult> GetAllForCurrentUserAsync()
+        {
+            var result = new SettingGroupResult();
+            var markTypeMap = new List<Type>
+            {
+                typeof(UserSettingMergeController),
+            };
+            foreach (var serviceType in _mergeOptions.UserSettingProviders
+                .Where(type => !markTypeMap.Any(markType => type.IsAssignableFrom(markType))))
+            {
+                var settingService = LazyServiceProvider.LazyGetRequiredService(serviceType).As<IUserSettingAppService>();
+                var currentResult = await settingService.GetAllForCurrentUserAsync();
+                foreach (var group in currentResult.Items)
+                {
+                    result.AddGroup(group);
+                }
+                markTypeMap.Add(serviceType);
+            }
+
+            return result;
+        }
+    }
+}
